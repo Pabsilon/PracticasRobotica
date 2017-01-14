ffb9da5d5d314095566030481b41294de5e89cb7
Pabsilon <pablomacveigh@gmail.com>
2017-01-14 11:58:24 +0000

    Añadido .gitignore, añadido script para compilar, añadidas funciones basicas

--- Theseus/Theseus.c
+++ Theseus/Theseus.c
@@ -1,5 +1,6 @@
 #include <wiringPi.h>
 #include <softPwm.h>
+#include <stdio.h>
 
 //BLOQUE DE DECLARACION DE CONSTANTES
 
@@ -22,18 +23,53 @@
 #define LS_GPIO 3
 #define RS_GPIO 4
 
+void forward_fast(){
+	printf("Going Forward Fast.\n");
+	softPwmWrite(LENGINE_GPIO,LENGINE_FORWARD_FAST);
+	softPwmWrite(RENGINE_GPIO,RENGINE_FORWARD_FAST);
+}
+
+void backwards_fast(){
+	printf("Going Backwards Fast.\n");
+	softPwmWrite(LENGINE_GPIO,LENGINE_BACKWARDS_FAST);
+	softPwmWrite(RENGINE_GPIO,RENGINE_BACKWARDS_FAST);
+}
+
+void forward_slow(){
+	printf("Going Forward Slow.\n");
+	softPwmWrite(LENGINE_GPIO,LENGINE_FORWARD_SLOW);
+	softPwmWrite(RENGINE_GPIO,RENGINE_FORWARD_SLOW);
+}
+
+void backwards_slow(){
+	printf("Going Backwards Slow.\n");
+	softPwmWrite(LENGINE_GPIO,LENGINE_BACKWARDS_SLOW);
+	softPwmWrite(RENGINE_GPIO,RENGINE_BACKWARDS_SLOW);
+}
+
+void hard_stop(){
+	printf("Hard Stopping.\n");
+	softPwmWrite(LENGINE_GPIO,ENGINE_STOP);
+	softPwmWrite(RENGINE_GPIO,ENGINE_STOP);
+}
+
+void hard_left(){
+	printf("Doing Hard Left. \n");
+	softPwmWrite(LENGINE_GPIO,LENGINE_BACKWARDS_FAST);
+	softPwmWrite(RENGINE_GPIO,RENGINE_FORWARD_FAST);
+}
+
+void hard_right(){
+	printf("Doing Hard Right. \n");
+	softPwmWrite(LENGINE_GPIO,LENGINE_FORWARD_FAST);
+	softPwmWrite(RENGINE_GPIO,RENGINE_BACKWARDS_FAST);
+	}
+
 int main(){
 	wiringPiSetup();
 	softPwmCreate(LENGINE_GPIO,0,200);
 	softPwmCreate(RENGINE_GPIO,0,200);
-	foward_fast();
+	forward_fast();
 	while(1){
 	}
-}
-
-void forward_fast(){
-	pthread_t left;
-	pthread_t right;
-	pthread_create(left,NULL,softPwmWrite(LENGINE_GPIO,LENGINE_FORWARD_FAST));
-	pthread_create(right,NULL,softPwmWrite(RENGINE_GPIO,RENGINE_FORWARD_FAST));
 }
---
+++ Theseus/compila.sh
@@ -0,0 +1,1 @@
+gcc -Wall -o Theseus Theseus.c -lwiringPi -pthread
