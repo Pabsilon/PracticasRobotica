2d7f0e09228dc3bc1ead2fc44f4c08a031c0c399
Pablo Mac-Veigh <pablomacveigh@gmail.com>
2017-01-16 18:47:19 +0100

    Ahora sigue lineas. Ahora podría resolver un laberinto espacioso.

--- Theseus/Theseus.c
+++ Theseus/Theseus.c
@@ -1,5 +1,5 @@
 /*
-Created by Pablo Mac-Veigh, for our Robotics class of 2016-2017.
+Created by Pablo Mac-Veigh and Javier Sésé, for our Robotics class of 2016-2017.
 
 This project uses the standard MIT license.
 
@@ -10,6 +10,7 @@ For the black line following, we still have to work on it
 */
 
 #include <wiringPi.h>
+#include <wiringPiSPI.h>
 #include <softPwm.h>
 #include <stdio.h>
 #include <time.h>
@@ -19,113 +20,101 @@ For the black line following, we still have to work on it
 
 //Engine Block
 //Engine Gpios
-#define LENGINE_GPIO 1
-#define RENGINE_GPIO 2
+#define LENGINE_GPIO 4
+#define RENGINE_GPIO 1
 //Engine frequencies.
 #define LENGINE_FORWARD_FAST 17
-#define LENGINE_FORWARD_SLOW 16
 #define ENGINE_STOP 15
-#define RENGINE_FORWARD_SLOW 14
 #define RENGINE_FORWARD_FAST 13
 //Opposite frequencies.
-#define LENGINE_BACKWARDS_FAST RENGINE_FORWARD_FAST
-#define LENGINE_BACKWARDS_SLOW RENGINE_FORWARD_SLOW
-#define RENGINE_BACKWARDS_FAST LENGINE_FORWARD_FAST
-#define RENGINE_BACKWARDS_SLOW LENGINE_FORWARD_SLOW
+#define LENGINE_BACKWARDS_FAST 13
+#define RENGINE_BACKWARDS_FAST 17
 
 //Light Sensor Block
-#define LS_GPIO 3
-#define RS_GPIO 4
+#define LS_GPIO 0
+#define RS_GPIO 2
 
 //Proximity Sensor Block
-#define SENSOR_CENTER 5
-#define SENSOR_SIDE 6
+#define SENSOR_CENTER 0
+#define SENSOR_SIDE 1
+#define SENSOR_SPICHANNEL 0
+#define SENSOR_CHANNELCONFIG 8
 
-#define SENSOR_CLOSE 250
-#define SENSOR_MEDIUM 400
-#define SENSOR_FAR 600
+//Sensor dinstances Block
+#define SENSOR_ACCEPTABLE_MIN 350
+#define SENSOR_ACCEPTABLE_MAX 450
+#define SENSOR_CENTER_TURN_DISTANCE 300
+#define SENSOR_CENTER_COLLISION_DISTANCE 650
+
+static int myFd;
+
+void loadSPIDriver(){
+	system("gpio load spi");
+}
+
+void spiSetup(int spiChannel){
+	myFd=wiringPiSPISetup (spiChannel, 1000000);
+}
 
 void waitFor (unsigned int seconds){
 	unsigned int retTime = time(0) + seconds;
 	while(time(0) < retTime);
 }
 
+int myAnalogRead(int spiChannel,int channelConfig,int analogChannel){
+	if (analogChannel<0 || analogChannel>7){
+		return -1;
+	}
+	unsigned char buffer[3] = {1};
+	buffer[1] = (channelConfig+analogChannel) <<4;
+	wiringPiSPIDataRW(spiChannel, buffer, 3);
+	return ( (buffer[1] & 3 ) <<8)+buffer[2];
+}
+
+int readProximity(int i){
+	return myAnalogRead(SENSOR_SPICHANNEL,SENSOR_CHANNELCONFIG,i);
+}
+
 void forward_fast(){
-	printf("Going Forward Fast.\n");
+//	printf("Going Forward Fast.\n");
 	softPwmWrite(LENGINE_GPIO,LENGINE_FORWARD_FAST);
 	softPwmWrite(RENGINE_GPIO,RENGINE_FORWARD_FAST);
 }
 
 void backwards_fast(){
-	printf("Going Backwards Fast.\n");
+//	printf("Going Backwards Fast.\n");
 	softPwmWrite(LENGINE_GPIO,LENGINE_BACKWARDS_FAST);
 	softPwmWrite(RENGINE_GPIO,RENGINE_BACKWARDS_FAST);
 }
 
-void forward_slow(){
-	printf("Going Forward Slow.\n");
-	softPwmWrite(LENGINE_GPIO,LENGINE_FORWARD_SLOW);
-	softPwmWrite(RENGINE_GPIO,RENGINE_FORWARD_SLOW);
-}
-
-void backwards_slow(){
-	printf("Going Backwards Slow.\n");
-	softPwmWrite(LENGINE_GPIO,LENGINE_BACKWARDS_SLOW);
-	softPwmWrite(RENGINE_GPIO,RENGINE_BACKWARDS_SLOW);
-}
-
 void hard_stop(){
-	printf("Hard Stopping.\n");
+//	printf("Hard Stopping.\n");
 	softPwmWrite(LENGINE_GPIO,ENGINE_STOP);
 	softPwmWrite(RENGINE_GPIO,ENGINE_STOP);
 }
 
 void hard_left(){
-	printf("Doing Hard Left. \n");
+//	printf("Doing Hard Left. \n");
 	softPwmWrite(LENGINE_GPIO,LENGINE_BACKWARDS_FAST);
 	softPwmWrite(RENGINE_GPIO,RENGINE_FORWARD_FAST);
 }
 
 void hard_right(){
-	printf("Doing Hard Right. \n");
+//	printf("Doing Hard Right. \n");
 	softPwmWrite(LENGINE_GPIO,LENGINE_FORWARD_FAST);
 	softPwmWrite(RENGINE_GPIO,RENGINE_BACKWARDS_FAST);
-	}
-
-void soft_left(){
-	printf("Doing Soft Left. \n");
-	softPwmWrite(LENGINE_GPIO,LENGINE_FORWARD_SLOW);
-	softPwmWrite(RENGINE_GPIO,RENGINE_FORWARD_FAST);
-}
-
-void soft_right(){
-	printf("Doing Soft Right.\n");
-	softPwmWrite(LENGINE_GPIO,LENGINE_FORWARD_FAST);
-	softPwmWrite(RENGINE_GPIO,RENGINE_FORWARD_SLOW);
 }
 
 void move_right(){
-	printf("Moving Right.\n");
+//	printf("Moving Right.\n");
 	softPwmWrite(LENGINE_GPIO,LENGINE_FORWARD_FAST);
 	softPwmWrite(RENGINE_GPIO,ENGINE_STOP);
 }
 
 void move_left(){
-	printf("Moving Left.\n");
+//	printf("Moving Left.\n");
 	softPwmWrite(LENGINE_GPIO,ENGINE_STOP);
 	softPwmWrite(RENGINE_GPIO,RENGINE_FORWARD_FAST);
-}
-
-void acute_left(){
-	printf("Doing Acute Left.\n");
-	softPwmWrite(LENGINE_GPIO,LENGINE_BACKWARDS_SLOW);
-	softPwmWrite(RENGINE_GPIO,RENGINE_FORWARD_FAST);
-}
-
-void acute_right(){
-	printf("Doing Acute Right.\n");
-	softPwmWrite(LENGINE_GPIO,LENGINE_FORWARD_FAST);
-	softPwmWrite(RENGINE_GPIO,RENGINE_BACKWARDS_SLOW);
 }
 
 /* Line Following Routine 
@@ -139,26 +128,28 @@ This is really rudimentary and should be worked on.
 void followLine(){
 	int left = 0;
 	int right = 0;
-	while ((!digitalRead(LS_GPIO))&&(!digitalRead(RS_GPIO))){
+	while ((digitalRead(LS_GPIO))&&(digitalRead(RS_GPIO))){
 		forward_fast();
 	}
-	while (digitalRead(LS_GPIO)){
+	while (!digitalRead(LS_GPIO)){
 		move_left();
+		printf("SENSOR IZQUIERO PISA NEGRO \n");
 		if (left==0){
 			left=1;
 		}
 	}
 	if(left==1){
-		waitFor(1);
+		//waitFor(1);
 	} 
-	while(digitalRead(RS_GPIO)){
+	while (!digitalRead(RS_GPIO)){
 		move_right();
+		printf("SENSOR DERECHO PISA NEGRO \n");
 		if (right==0){
 			right=1;
 		}
 	}
 	if (right==1){
-		waitFor(1);
+		//waitFor(1);
 	}
 	left=0;
 	right=0;
@@ -168,24 +159,52 @@ void followLine(){
 
 //We will try to solve a maze using the left(or right)hand algorithm
 // If side = 0; it's left hand, if side =1; it's right hand.
-void solveMaze(int side){
-
-//While the sensor on the side <SENSOR_CLOSE , keep forward.
-//If it changes, turn 90º to the SENSOR SIDE.
-//If it doesn't change, and the front sensor becomes close, turn to opposite sensor side 90º
+void solveMaze(){
+	while (readProximity(SENSOR_CENTER)>SENSOR_CENTER_COLLISION_DISTANCE){
+		backwards_fast();
+	}
+	//Move Forward
+	while (SENSOR_ACCEPTABLE_MAX>readProximity(SENSOR_SIDE)&&readProximity(SENSOR_SIDE)>SENSOR_ACCEPTABLE_MIN){
+		forward_fast();
+		//Turn in case of obstacle in front
+		while (readProximity(SENSOR_CENTER)>SENSOR_CENTER_TURN_DISTANCE&&(readProximity(SENSOR_CENTER)<SENSOR_CENTER_COLLISION_DISTANCE)){
+			move_right();
+		}
+		while(readProximity(SENSOR_CENTER)>SENSOR_CENTER_COLLISION_DISTANCE){
+			backwards_fast();
+		}
+	}
+	//Correct vector to wall, right drifting
+	while ((SENSOR_ACCEPTABLE_MAX>readProximity(SENSOR_SIDE)&&readProximity(SENSOR_SIDE)<SENSOR_ACCEPTABLE_MIN)){
+		move_left();
+		//If we find an object in our path while correcting we turn to the right
+		if (readProximity(SENSOR_CENTER)>SENSOR_CENTER_TURN_DISTANCE&&(readProximity(SENSOR_CENTER)<SENSOR_CENTER_COLLISION_DISTANCE)){
+			move_right();
+		}
+		while(readProximity(SENSOR_CENTER)>SENSOR_CENTER_COLLISION_DISTANCE){
+			backwards_fast();
+		}
+	}
+	//Correct vector to wall, left drifting
+	while (SENSOR_ACCEPTABLE_MAX< readProximity(SENSOR_SIDE)&&readProximity(SENSOR_SIDE)>SENSOR_ACCEPTABLE_MIN){
+		move_right();
+		while(readProximity(SENSOR_CENTER)>SENSOR_CENTER_COLLISION_DISTANCE){
+			backwards_fast();
+		}
+	}
+	solveMaze();
 
 }
 
 int main(){
 	//wiringPi initialization, necessary.
 	wiringPiSetup();
+	loadSPIDriver();
+	spiSetup(0);
 	//PwmCreation, necesarry for each GPIO in order to send signals.
 	//The wiringPi documentation says each softPwmCreate uses 0.5% of the CPU, so it should be safe
 	softPwmCreate(LENGINE_GPIO,0,200);
 	softPwmCreate(RENGINE_GPIO,0,200);
-	
-	followLine();
-
-	while(1){
-	}
+	//followLine()
+	solveMaze();
 }
